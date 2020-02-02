31e3ab98cf13ce5fee7adc7de3c57f5ac8b5ebf4
River Bryant <river.bryant@ibethel.org>
2020-02-01 20:00:11 -0800

    update ip address

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '3.87.74.113',
+      host: '54.242.81.23',
       username: 'ubuntu',
       privateKey: 'hs2-key.pem'
     })
